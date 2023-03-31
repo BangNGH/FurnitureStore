943874cd6d5e413d25f2d917cc24effcf6abf658
= <=>
2023-03-31 23:07:09 +0700

    update sqlquery

---
+++ SQLQuery_v1.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE furniture_store;
+USE furniture_store;
+
+
+
+CREATE TABLE Customers (
+  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  name NVARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  email VARCHAR(255)
+);
+
+CREATE TABLE Employees (
+  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  email VARCHAR(255)
+);
+CREATE TABLE Products (
+  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  name NVARCHAR(255) NOT NULL,
+  description nvarchar(MAX),
+  price DECIMAL(10,2) NOT NULL
+);
+CREATE TABLE Invoices (
+  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  date DATETIME NOT NULL,
+  customer_id INT NOT NULL FOREIGN KEY REFERENCES Customers(id),
+  employee_id INT NOT NULL FOREIGN KEY REFERENCES Employees(id)
+);
+
+CREATE TABLE InvoiceDetails (
+  invoice_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (invoice_id, product_id),
+  FOREIGN KEY (invoice_id) REFERENCES Invoices(id),
+  FOREIGN KEY (product_id) REFERENCES Products(id)
+);
+
+CREATE TABLE ProductCategories (
+  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  name NVARCHAR(255) NOT NULL,
+);
+ALTER TABLE Products ADD category_id INT NOT NULL FOREIGN KEY REFERENCES ProductCategories(id);
+
